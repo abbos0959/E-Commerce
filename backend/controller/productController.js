c78757a544dcef7dc3214554ab2434db6dd7aefe
abbos0959 <abbosakromovich777@gmail.com>
2022-07-21 05:06:43 -0700

    product

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,10 +1,44 @@
-const getAllProducts=async(req,res)=>{
+const ProductModel = require("../model/productModel");
 
+// hamma pproductlarni chiqazish
+const getAllProducts = async (req, res) => {
+   const data = await ProductModel.find();
 
-    res.status(200).json({
-        message:"route yaratildi"
-    })
-}
+   res.status(200).json({
+      data,
+      message: "route yaratildi",
+   });
+};
 
+//bitta productni id orqali topish
 
-module.exports={getAllProducts}
+const getProduct = async (req, res) => {
+   const data = await ProductModel.findById(req.params.id);
+   res.status(200).json({
+      data,
+   });
+};
+
+//yangi product yaratish admin
+const createProduct = async (req, res) => {
+   const data = await ProductModel.create(req.body);
+   res.status(201).json({
+      data,
+   });
+};
+const deleteProduct = async (req, res) => {
+   const data = await ProductModel.findByIdAndDelete(req.params.id);
+   res.status(200).json({
+      message: "product o`chirildi",
+   });
+};
+
+const UpdateProduct = async (req, res) => {
+   const data = await ProductModel.findByIdAndUpdate(req.params.id, req.body);
+
+   res.status(200).json({
+      message: "update product",
+      data,
+   });
+};
+module.exports = { getAllProducts, getProduct, createProduct, deleteProduct, UpdateProduct };
--- backend/router/productRouter.js
+++ backend/router/productRouter.js
@@ -1,6 +1,8 @@
 const express=require("express")
 const router=express.Router()
 const Products=require("../controller/productController")
-router.route("/products").get(Products.getAllProducts)
+router.route("/products").get(Products.getAllProducts).post(Products.createProduct)
+router.route("/products/:id").get(Products.getProduct)
+router.route("/products/:id").delete(Products.deleteProduct).patch(Products.UpdateProduct)
 
 module.exports=router
