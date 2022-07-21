23f1ea45a9025cd8bdb0921684a97a2941d08ae6
abbos0959 <abbosakromovich777@gmail.com>
2022-07-21 12:30:05 -0700

    advanced filter

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -21,9 +21,14 @@ class ApiFeatures {
       const querCopy = { ...this.queryStr };
       const remove = ["abbos", "limit", "page"];
       remove.forEach((i) => delete querCopy[i]);
-      this.query = this.query.find(querCopy);
+
+      //advanced filter qilish
+      let queryStr = JSON.stringify(querCopy);
+      queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+      this.query = this.query.find(JSON.parse(queryStr));
       return this;
    }
+   pagination() {}
 }
 
 module.exports = ApiFeatures;
