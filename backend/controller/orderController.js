119221de795a7a07341a346328d969a8d6c926d4
abbos0959 <abbosakromovich777@gmail.com>
2022-08-05 13:14:33 -0700

    allorders

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -34,9 +34,42 @@ const newOrder = catchErrorAsync(async (req, res, next) => {
 
 // bitta orderni olish
 
-const getSingleOrder=catchErrorAsync(async(req,res,next)=>{
-   const order=await Order.findById()
+const getSingleOrder = catchErrorAsync(async (req, res, next) => {
+   const order = await Order.findById(req.params.id).populate({
+      path: "user",
+      select: "name email ",
+   });
+   if (!order) {
+      return next(new AppError("bunday Idli Order mavjud emas", 404));
+   }
+   res.status(200).json({
+      status: true,
+      order,
+   });
+});
 
-})
+//login qilib kirgan user barcha orderlari
+const meOrder = catchErrorAsync(async (req, res, next) => {
+   const orders = await Order.find({ user: req.user._id });
 
-module.exports = { newOrder,getSingleOrder };
+   res.status(200).json({
+      status: true,
+      orders,
+   });
+});
+// hamma orderlarni  ko`rish
+const getAllOrders = catchErrorAsync(async (req, res, next) => {
+   const orders = await Order.find();
+
+   let totalAmount = 0;
+   orders.forEach((val) => {
+      totalAmount += val.totalPrice;
+   });
+   res.status(200).json({
+      status: true,
+      totalAmount,
+      orders,
+   });
+});
+
+module.exports = { newOrder, getSingleOrder, meOrder, getAllOrders };
--- backend/router/orderRouter.js
+++ backend/router/orderRouter.js
@@ -4,5 +4,7 @@ const OrderController = require("../controller/orderController");
 const { Isauthentication, authoriseRoles } = require("../middleware/isAuthentication");
 
 router.route("/order/new").post(Isauthentication, OrderController.newOrder);
+router.route("/order/:id").get(Isauthentication,  OrderController.getSingleOrder);
+router.route("/orders/me").get(Isauthentication, OrderController.meOrder);
 
-module.exports=router
+module.exports = router;
