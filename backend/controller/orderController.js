e35d52beb2a3e86ea6c6a2703224abf98f5e874e
abbos0959 <abbosakromovich777@gmail.com>
2022-08-05 22:09:28 -0700

    deleteOrder

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -50,7 +50,10 @@ const getSingleOrder = catchErrorAsync(async (req, res, next) => {
 
 //login qilib kirgan user barcha orderlari
 const meOrder = catchErrorAsync(async (req, res, next) => {
-   const orders = await Order.find({ user: req.user._id });
+   const orders = await Order.find({ user: req.user._id }).populate({
+      path: "orderItems.product",
+      select: "name -_id",
+   });
 
    res.status(200).json({
       status: true,
@@ -72,4 +75,49 @@ const getAllOrders = catchErrorAsync(async (req, res, next) => {
    });
 });
 
-module.exports = { newOrder, getSingleOrder, meOrder, getAllOrders };
+//orderlarni yangilash
+const updateOrder = catchErrorAsync(async (req, res, next) => {
+   const order = await Order.findById(req.params.id);
+
+   if (!order) {
+      return next(new AppError("bunday Idli mahsulot mavjud emas", 404));
+   }
+   if (order.orderStatus === "Delivered") {
+      return next(new AppError(" bu buyurtma sizga allaqachon yetkazib berilgan", 404));
+   }
+
+   order.orderItems.forEach(async (order) => {
+      await updateStock(order.product, order.quantity);
+   });
+   order.orderStatus = req.body.status;
+   if (req.body.status == "Delivered") {
+      order.deliveredAt = Date.now();
+   }
+   await order.save({ validateBeforeSave: false });
+
+   res.status(200).json({
+      success: true,
+   });
+});
+
+async function updateStock(id, quantity) {
+   const product = await ProductModel.findById(id);
+   if (product.stock >= 0) {
+      product.stock -= quantity;
+   }
+   await product.save({ validateBeforeSave: false });
+}
+
+//delete order
+const deleteOrder = catchErrorAsync(async (req, res, next) => {
+   const order = await Order.findById(req.params.id);
+
+   if (!order) {
+      return next(new AppError("bunday Idli product mavjud emas", 404));
+   }
+   await order.remove();
+   res.status(200).json({
+      status: true,
+   });
+});
+module.exports = { newOrder, getSingleOrder, meOrder, getAllOrders, updateOrder, deleteOrder };
--- backend/router/orderRouter.js
+++ backend/router/orderRouter.js
@@ -4,7 +4,14 @@ const OrderController = require("../controller/orderController");
 const { Isauthentication, authoriseRoles } = require("../middleware/isAuthentication");
 
 router.route("/order/new").post(Isauthentication, OrderController.newOrder);
-router.route("/order/:id").get(Isauthentication,  OrderController.getSingleOrder);
+router.route("/order/:id").get(Isauthentication, OrderController.getSingleOrder);
 router.route("/orders/me").get(Isauthentication, OrderController.meOrder);
+router
+   .route("/admin/orders")
+   .get(Isauthentication, authoriseRoles("admin"), OrderController.getAllOrders);
+router
+   .route("/admin/order/:id")
+   .patch(Isauthentication, authoriseRoles("admin"), OrderController.updateOrder)
+   .delete(Isauthentication, authoriseRoles("admin"), OrderController.deleteOrder);
 
 module.exports = router;
